67eb5749f4a511efc8d41d2067a7fa7702e2169d
smbalaji-grl <balaji.m@graniteriverlabs.in>
2024-01-24 22:31:23 +0530

    Events updating and how components will be executed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,19 @@ import "./ExpenseItem.css"
 
 export default function ExpenseItem(props){
 
+let title_= props.title;
+const clickHandler=()=>{
+title_= "Updated";
+console.log("it is real updated?");
+};
     return(
         <Card className="expense-item">
         <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title_}</h2>
             <div className="expense-item__price">${props.amount}</div>
          </div>
+         <button onClick={clickHandler}> change title </button>
     </Card>
   );
 }
