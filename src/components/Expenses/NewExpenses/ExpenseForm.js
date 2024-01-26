e18c511237279da00108bdbfd74742ab3e59536d
smbalaji-grl <balaji.m@graniteriverlabs.in>
2024-01-26 14:21:10 +0530

    Working with multiple states

--- src/components/Expenses/NewExpenses/ExpenseForm.js
+++ src/components/Expenses/NewExpenses/ExpenseForm.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
-const titleChangeHandler = (event) => {
-  console.log(event.target.value);
-};
-
 const ExpenseForm = () => {
+  //The useState hook should be called at the top level of your functional component and not inside the event handler functions.
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enterAmount, setEnteredAmount] = useState("");
+  const [enterDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    console.log(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
@@ -14,11 +30,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__controls">
           <label>amount</label>
-          <input type="number" min={0.01} step={0.01} />
+          <input
+            type="number"
+            min={0.01}
+            step={0.01}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__controls">
           <label>Date</label>
-          <input type="date" min={"2023-01-01"} max={"2024-01-26"} />
+          <input
+            type="date"
+            min={"2023-01-01"}
+            max={"2024-01-26"}
+            onChange={dateChangeHandler}
+          />
         </div>
         <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
