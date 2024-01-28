1e0a3e6a9443a8884c004e43ac1f4ea5cd75f617
smbalaji-grl <balaji.m@graniteriverlabs.in>
2024-01-28 14:06:47 +0530

    two way binding- after refreshing set to defaultvalue

--- src/components/Expenses/NewExpenses/ExpenseForm.js
+++ src/components/Expenses/NewExpenses/ExpenseForm.js
@@ -3,37 +3,87 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
   //The useState hook should be called at the top level of your functional component and not inside the event handler functions.
+  // below are using individal state
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enterAmount, setEnteredAmount] = useState("");
   const [enterDate, setEnteredDate] = useState("");
 
+  // const [userInput_, setUserInput_] = useState({
+  //   enteredTitle: "",
+  //   enterAmount: "",
+  //   enterDate: "",
+  // });
+
   const titleChangeHandler = (event) => {
+    //below is updating state but before taking or saving old state screenshot and updating or overide value
+    //setUserInput_({ ...userInput_, enteredTitle: event.target.value });
+
+    // this method is alternative. since react update state by scheulding. pass function to state function
+    //it will first return previous sate and update new one with instant (reacts gaurted)
+    // setUserInput_((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+
     setEnteredTitle(event.target.value);
     console.log(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
+    //setUserInput_({ ...userInput_, enterAmount: event.target.value });
     setEnteredAmount(event.target.value);
+
+    // setUserInput_((prevState) => {
+    //   return { ...prevState, enterAmount: event.target.value };
+    // });
     console.log(event.target.value);
   };
   const dateChangeHandler = (event) => {
+    // setUserInput_({ ...userInput_, enterDate: event.target.value });
     setEnteredDate(event.target.value);
+
+    // setUserInput_((prevState) => {
+    //   return { ...prevState, enterDate: event.target.value };
+    // });
     console.log(event.target.value);
   };
 
+  const inputChangeHandler = (Identifier, value) => {
+    if ((Identifier === "title", value)) setEnteredTitle(value);
+    else if (Identifier === "amount") setEnteredAmount(value);
+    else setEnteredDate(value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      tittle: enteredTitle,
+      amount: enterAmount,
+      Date: new Date(enterDate),
+    };
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__controls">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            //onChange={(event)=>inputChangeHandler("title",event.target.value)} />
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__controls">
           <label>amount</label>
           <input
             type="number"
+            value={enterAmount}
             min={0.01}
             step={0.01}
+            // onChange={(event)=> inputChangeHandler("amount",event.target.value)}
             onChange={amountChangeHandler}
           />
         </div>
@@ -41,8 +91,10 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input
             type="date"
+            value={enterDate}
             min={"2023-01-01"}
             max={"2024-01-26"}
+            // onChange={(event)=> inputChangeHandler("date",event.target.value)}
             onChange={dateChangeHandler}
           />
         </div>
